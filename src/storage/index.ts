4b11abad60e3b093a88ea4cc88f11a19059da633
jayisaac0 <jayisaac0@gmail.com>
2022-11-18 08:44:12 +0300

    temporary data store

---
+++ src/storage/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Assuming the vending machine has one item in it
+ */
+export let vending = [
+    { slot: "coke", price: 2, items: 5 }
+];
+
+/**
+ *  Hold denominations and available count
+ */
+export let denominations = [
+    {denomination: 1, coins: 1}, 
+    {denomination: 2, coins: 3}, 
+    {denomination: 5, coins: 5},
+];
